c3606a73c37b6dbd5103ba2e45aaa795b377d3d0
Thomas Heslin <tjheslin1@gmail.com>
2018-12-15 14:23:46 +0000

    Adding goblin Show instance

--- src/main/scala/io/github/tjheslin1/dmspredictor/monsters/Goblin.scala
+++ src/main/scala/io/github/tjheslin1/dmspredictor/monsters/Goblin.scala
@@ -1,6 +1,7 @@
 package io.github.tjheslin1.dmspredictor.monsters
 
 import cats.Show
+import cats.syntax.show._
 import eu.timepit.refined.auto._
 import io.github.tjheslin1.dmspredictor.model._
 import io.github.tjheslin1.dmspredictor.util.IntOps._
@@ -13,7 +14,8 @@ case class Goblin(health: Int,
                   weapon: Weapon,
                   override val resistances: List[DamageType] = List(),
                   override val immunities: List[DamageType] = List(),
-                  override val name: String = NameGenerator.randomName) extends Creature {
+                  override val name: String = NameGenerator.randomName)
+    extends Creature {
 
   val creatureType: CreatureType = Monster
 
@@ -27,7 +29,11 @@ object Goblin {
   def levelOneGoblin[_: RS](weapon: Weapon = Shortsword): Goblin =
     Goblin(calculateHealth, BaseStats(8, 14, 10, 10, 8, 8), 15, weapon)
 
-//  implicit val goblinShow: Show[Goblin] = Show.show { goblin =>
-//    s"Goblin - ${goblin.creature.show}}"
-//  }
+  implicit val goblinShow: Show[Goblin] = Show.show { goblin =>
+    s"Fighter: " +
+      s"Name: ${goblin.name}, " +
+      s"health: ${goblin.health}, " +
+      s"AC: ${goblin.armourClass}, " +
+      s"${goblin.weapon.show}"
+  }
 }
