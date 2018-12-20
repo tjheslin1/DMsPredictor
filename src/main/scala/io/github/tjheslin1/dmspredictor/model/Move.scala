8fd1acc5032900003b6313e5b1d1af06c407d5d3
Thomas Heslin <tjheslin1@gmail.com>
2018-12-20 16:42:30 +0000

    Working ability in Move

--- src/main/scala/io/github/tjheslin1/dmspredictor/model/Move.scala
+++ src/main/scala/io/github/tjheslin1/dmspredictor/model/Move.scala
@@ -1,7 +1,6 @@
 package io.github.tjheslin1.dmspredictor.model
 
 import cats.syntax.option._
-import com.typesafe.scalalogging.LazyLogging
 import io.github.tjheslin1.dmspredictor.classes.Fighter
 import io.github.tjheslin1.dmspredictor.model.Actions.attackAndDamage
 import io.github.tjheslin1.dmspredictor.strategy._
@@ -10,7 +9,7 @@ import io.github.tjheslin1.dmspredictor.util.QueueOps._
 import scala.collection.immutable.Queue
 import scala.util.{Random => JRandom}
 
-object Move extends LazyLogging {
+object Move {
 
   def takeMove[_: RS](queue: Queue[Combatant], focus: Focus): Queue[Combatant] = {
     val (combatant, others) = queue.dequeue
@@ -22,25 +21,28 @@ object Move extends LazyLogging {
       val pcToAttack  = nextToFocus(pcs, focus)
 
       val updatedCombatants = if (combatant.creature.creatureType == PlayerCharacter) {
-        combatant.creature match {
-          case fighter: Fighter =>
-            val classAbilities = implicitly[ClassAbilities[Fighter]].abilities.sortBy{ case (priority, _) => priority }
-            val optAbility = classAbilities.find { case (_, fighterAbility) =>
 
-              val ability = fighterAbility(fighter)
-              ability.conditionMet && ability.triggerMet
-            }
+        def abilities[T <: Creature](cmb: T)(implicit ca: ClassAbilities[T]): List[(Int, T => Ability[T])] = ca.abilities
 
-            mobToAttack.fold(none[(Combatant, Combatant)]) { mob =>
-              optAbility.fold(attackAndDamage(combatant, mob).some) {
-                case (_, ability) =>
-                  val actedFighter = ability(fighter).useAbility
-                  val updatedCombatant = combatant.copy(creature = ability(actedFighter).update)
+        val classAbilities: List[(Int, Creature => Ability[Creature])] = combatant.creature match {
+          case f: Fighter => abilities(f)(implicitly[ClassAbilities[Fighter]]).asInstanceOf[List[(Int, Creature => Ability[Creature])]]
+          case _          => List.empty[(Int, Creature => Ability[Creature])]
+        }
 
-                  (updatedCombatant, mob).some
-              }
-            }
-          case _ => none[(Combatant, Combatant)]
+        val optAbility = classAbilities.find {
+          case (_, fighterAbility) =>
+            val ability = fighterAbility(combatant.creature)
+            ability.conditionMet && ability.triggerMet
+        }
+
+        mobToAttack.fold(none[(Combatant, Combatant)]) { mob =>
+          optAbility.fold(attackAndDamage(combatant, mob).some) {
+            case (_, ability) =>
+              val actedFighter     = ability(combatant.creature).useAbility
+              val updatedCombatant = combatant.copy(creature = ability(actedFighter).update)
+
+              (updatedCombatant, mob).some
+          }
         }
       } else {
         pcToAttack.fold(none[(Combatant, Combatant)])(attackAndDamage(combatant, _).some)
@@ -56,6 +58,7 @@ object Move extends LazyLogging {
   }
 
   private def nextToFocus(combatants: Queue[Combatant], focus: Focus): Option[Combatant] = {
+
     val consciousCombatants = combatants.filter(_.creature.isConscious)
     focus match {
       case LowestFirst => consciousCombatants.sortBy(_.creature.health).headOption
